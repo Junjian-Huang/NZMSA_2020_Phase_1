a02a594ee3082a643c4e3938407a1d92fa554bab
Junjian Huang <junjian.huang@hotmail.com>
2020-07-15 19:12:24 +1200

    create a search bar and button

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './Components/SearchBarComponent/SearchBar'
 
 function App() {
   return (
     <div className="App">
-   
+      <SearchBar/>
     </div>
   );
 }
